4f69e66a2d26b99f93c54128876758984ffb876b
glob-bruh <34488272+glob-bruh@users.noreply.github.com>
2026-08-24 23:13:26 -0600

    Updates styles.

--- blog/script.js
+++ blog/script.js
@@ -235,6 +235,7 @@ function attachUsageTxt() {
   var mdElem = document.querySelector("#MARKDOWN-CONTENT-HERE");
   mdElem.innerHTML = x;
   collapseParser(); // THIS CONVERTS THE DROPDOWN TAGS TO ACTUAL DROPDOWNS
+  document.getElementById("preventPeopleFromGoingBlind").remove();
   return;
 }
 
